113e5b37180f2f775e9c08cabbf2d46d359643c0
Chris Penny <chris.penny@gmail.com>
2022-02-21 09:35:14 +1300

    Linting

--- src/RelationshipGraph/GraphBuildException.php
+++ src/RelationshipGraph/GraphBuildException.php
@@ -7,7 +7,7 @@ use Throwable;
 
 class GraphBuildException extends Exception
 {
-    public function __construct(array $errors, int $code = 0, Throwable $previous = null)
+    public function __construct(array $errors, int $code = 0, ?Throwable $previous = null)
     {
         $message = sprintf("\r\n%s", implode("\r\n", $errors));
 
